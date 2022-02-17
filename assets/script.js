1fa6641c010ec193cb0185c22a67b519c0ea410a
Princess Johnson <princessjohnson@Princesss-MacBook-Pro.local>
2022-02-16 21:21:17 -0500

    lrihogh#

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,86 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Princess Johnson" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+// gb=Gingerbread
+ // cc=Chocolate Chip
+// ss=Sugar Sprinkle
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
+// sadd
 const gbPlusBtn = document.querySelector('#add-gb')
+const ccPlusBtn = document.querySelector('#add-cc')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+// minus 
+const ccMinusBtn = document.querySelector('#minus-cc')
+const gbMinusBtn = document.querySelector('#minus-gb')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
+   let gb = (function () {
+        let gb = 0;
+        return function () {gb += 1; return gb;}
+    })();
+           function myFunction (){
+        document.getElementById("qty-gb").innerHTML += gb();
+      };
 // HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+console.log (myFunction (".qty-gb")
 
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
 
-// TODO: Hook up event listeners for the rest of the buttons
+)})
+
+ccPlusBtn.addEventListener('click', function() {
+    let cc = (function () {
+         let cc = 0;
+         return function () {cc += 1; return cc;}
+     })();
+            function myFunction (){
+         document.getElementById("qty-cc").innerHTML += cc();
+       };
+ // HINT: You can delete this console.log after you no longer need it!
+ console.log (myFunction (".qty-cc")
+ 
+
+ 
+ )})
+
+ sugarPlusBtn.addEventListener('click', function() {
+    let sugar = (function () {
+         let sugar = 0;
+         return function () {sugar += 1; return sugar;}
+     })();
+            function myFunction (){
+         document.getElementById("qty-sugar").innerHTML += sugar();
+       };
+ // HINT: You can delete this console.log after you no longer need it!
+ console.log (myFunction (".qty-sugar")
+ )})
+
+
+ sugarMinusBtn.addEventListener('click', function() {
+    let sugar = (function () {
+         let sugar = 0;
+         return function () {sugar = 1-1 ; return sugar;}
+     })();
+            function myFunction (){
+         document.getElementById("qty-sugar").innerHTML = sugar();
+       };
+ // HINT: You can delete this console.log after you no longer need it!
+ console.log (myFunction (".qty-sugar")
+ 
+
+ )})
+ 
+
+
+    
+   
